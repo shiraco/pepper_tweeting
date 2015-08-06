38d1b87db88fdca1a4917e7a77f1525886aae4f3
Koji Shiraishi <shiraco@gmail.com>
2015-08-06 10:09:40 +0900

    Update readme

--- pepper_tweeting.py
+++ pepper_tweeting.py
@@ -2,6 +2,7 @@
 
 from naoqi import ALProxy
 
+import sys
 import json
 import re
 
@@ -12,6 +13,11 @@ from tweepy import Stream
 from settings import consumer_key, consumer_secret, access_token, access_token_secret
 
 class StdOutListener(StreamListener):
+
+    def __init__(self, pepper_host, pepper_port):
+        self.pepper_host = pepper_host
+        self.pepper_port = pepper_port
+
     def on_data(self, data):
         d = json.loads(data)
         raw_tweet = d["text"]
@@ -19,7 +25,7 @@ class StdOutListener(StreamListener):
         tweet = Cleanser.cleanse(raw_tweet)
         print("%s -> %s" % (raw_tweet, tweet))
 
-        tts = ALProxy("ALTextToSpeech", "192.168.1.98", 9559)
+        tts = ALProxy("ALTextToSpeech", self.pepper_host, self.pepper_port)
         tts.say(str(tweet.encode("utf-8")))
 
         return True
@@ -27,27 +33,30 @@ class StdOutListener(StreamListener):
     def on_error(self, status):
         print(status)
 
-class Cleanser():
+class Cleanser(object):
 
     @classmethod
     def cleanse(cls, tweet):
-
         # remove RT:
-        tweet = re.sub(r'(RT)', ' ', tweet)
+        tweet = re.sub(r"(RT)", " ", tweet)
         # remove URL
-        tweet = re.sub(r'(?:^|[\s　]*)((?:https?|ftp)://[\w/:%#\$&\?\(\)~\.=\+\-]+)', ' ', tweet)
+        tweet = re.sub(r"(?:^|[\s　]*)((?:https?|ftp)://[\w/:%#\$&\?\(\)~\.=\+\-]+)", " ", tweet)
         # remove hash tag
-        tweet = re.sub(r'\s#([a-zA-Z0-9_]+)', ' ', tweet)
+        tweet = re.sub(r"\s#([a-zA-Z0-9_]+)", " ", tweet)
         # remove screen name
-        tweet = re.sub(r'(@[A-Za-z0-9_]{1,15})', ' ', tweet)
+        tweet = re.sub(r"(@[A-Za-z0-9_]{1,15})", " ", tweet)
 
         return tweet
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    pepper_host = sys.argv[1]  # e.g. "192.168.0.2"
+    hash_tag = sys.argv[2]  # e.g. ""#pepper_say"
+    pepper_port = 9559
+
     l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
     stream = Stream(auth, l)
-    stream.filter(track=['#pepper_say'])
+    stream.filter(track=[hash_tag])
